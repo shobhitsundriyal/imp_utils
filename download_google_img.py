d907781c5db5f4ec1d7cdccce10f96d3c97a73d3
Shobhit Sundriyal <35799284+shobhitsundriyal@users.noreply.github.com>
2020-01-08 11:35:27 +0530

    Update download_google_img.py

--- download_google_img.py
+++ download_google_img.py
@@ -4,7 +4,7 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-#maximum limit is 100 else need chromedriver
+#maximum limit is 100 images
 arguments = {"keywords":"search keywords","limit":100,"print_urls":True, "chromedriver":'\..'}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
